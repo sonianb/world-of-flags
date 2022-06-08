be5e8c0a79120548caac81d3bb1863bbb6c25214
root <sonia.botsarova@gmail.com>
2022-06-08 15:09:16 +0100

    create file structure
    Co-authored-by: Asmahan <AsmahanM@users.noreply.github.com>

--- week-sonia-asmahan/src/App.jsx
+++ week-sonia-asmahan/src/App.jsx
@@ -1,41 +1,36 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    //Flags.jsx --> will contain question and flag function(fetch API & generate data), display "loading" msg & remember to export
+    //Answers.jsx --> display correct answer and 2 wrong answers. 
+
+    //To generate wrong ones: create an array with all 195 countries, create randomiser, 
+    //randomiser will give random country that will have validity attribute wrong, display wrong answer.
+
+    //HandleAnswer --> check answer validity, change backgound color to green if correct, red if wrong
+
+    //Score.jsx --> function to count correct answers and display the result 
+
+    //Lives.jsx --> function to track wrong answers, when we reach 3 wrong answers --> display game over & try again btn
+
+    //NextButton.jsx --> generate new flag & answers, keep the state of score and lives
+
+    //EndGame.jsx --> show "game over" and "play again" button.
+    //Play again btn will reset the state of the game(score:0, lives:3)
+
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Guess the country</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
